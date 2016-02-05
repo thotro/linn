bee86ba3357dd3b1c1a2caa5fd5162626c697543
Thomas Trojer <thomas.trojer@uibk.ac.at>
2016-02-05 01:11:16 +0100

    api doc + example progress

--- linn.examples/src/main/java/linn/examples/BasicExample.java
+++ linn.examples/src/main/java/linn/examples/BasicExample.java
@@ -20,11 +20,11 @@ public class BasicExample extends PApplet {
 		final Linn linn = LinnBuilder.newLinn("BasicExample")
 				.withAuthor("Thomas Trojer")
 				// rule 1)
-				.withRule("H").andWeight(5.5).andProduction().F().rewrite("H")
+				.withRule("H").andWeight(2).andProduction().F(10).rewrite("H")
 				.done()
 				// rule 2)
-				.withRule("H").andWeight(0.5).andProduction().F().branch().F()
-				.done().done()
+				.withRule("H").andWeight(1).andProduction().F(20).rewrite("H")
+				.done()
 				// finalize
 				.build();
 		/*
@@ -44,14 +44,28 @@ public class BasicExample extends PApplet {
 	@Override
 	public void settings() {
 		this.size(800, 600);
+		this.noLoop();
+	}
+
+	@Override
+	public void mouseClicked() {
+		super.mouseClicked();
+		this.redraw();
 	}
 
 	@Override
 	public void draw() {
 		LinnTurtle beforeState = this.linnExecutor.getState();
-		this.linnExecutor.executeAtMost(1);
+		this.linnExecutor.executePartial(p -> {
+			System.out.println("fill");
+			this.fill(255, 255, 255);
+		});
 		LinnTurtle afterState = this.linnExecutor.getState();
 		System.out.println(beforeState + " -- " + afterState);
+		this.line(400 + (float) beforeState.getX(),
+				300 + (float) beforeState.getY(),
+				400 + (float) afterState.getX(),
+				300 + (float) afterState.getY());
 	}
 
 	public static void main(String _args[]) {
