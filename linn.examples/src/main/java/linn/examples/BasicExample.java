50c61e7768ce4b82e650c8bc59c63d25759db0d3
Thomas Trojer <thomas.trojer@uibk.ac.at>
2016-02-22 23:06:22 +0100

    modified examples

--- linn.examples/src/main/java/linn/examples/BasicExample.java
+++ linn.examples/src/main/java/linn/examples/BasicExample.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2016 by Thomas Trojer <thomas@trojer.net>
+ * LINN - A small L-System interpreter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package linn.examples;
 
 import linn.core.Linn;
@@ -6,6 +23,17 @@ import linn.core.execute.state.LinnTurtle;
 import linn.core.lang.LinnBuilder;
 import processing.core.PApplet;
 
+/**
+ * A 2D processing sketch that draws a quite boring circular pattern. This
+ * sketch features
+ * <ul>
+ * <li>new L-system production iteration every 250ms</li>
+ * <li>state-change handler based rendering</li>
+ * {@link LinnTurtle}</li>
+ * </ul>
+ *
+ * @author Thomas Trojer <thomas@trojer.net> -- Initial contribution
+ */
 public class BasicExample extends PApplet {
 
 	private LinnExecutor linnExecutor;
@@ -14,7 +42,7 @@ public class BasicExample extends PApplet {
 	public void setup() {
 		final Linn linn = LinnBuilder.newLinn("BasicExample").withAuthor("Thomas Trojer")
 				// rule: H ---> ROTATE MOVE-100 H
-				.withRule("H").andProduction().yaw(1.0f).F(100).rewrite("H").done()
+				.withRule("H").andProduction().yaw(1.0f).F(200).rewrite("H").done()
 				// finalize
 				.build();
 		// configuring the execution environment
@@ -25,21 +53,19 @@ public class BasicExample extends PApplet {
 			}
 			// connect previous and current position with a line
 			LinnTurtle tp = t.getPreviousState();
-			this.line(400 + (float) tp.getX(), 300 + (float) tp.getY(), 400 + (float) t.getX(), 300 + (float) t.getY());
+			this.line(200 + (float) tp.getX(), 400 + (float) tp.getY(), 200 + (float) t.getX(), 400 + (float) t.getY());
 		})
 				// axiom to start the L-System with: H
 				.withAxiom().rewrite("H").done();
+
+		// set ultra-slow frame rate
+		this.frameRate(4);
 	}
 
 	@Override
 	public void settings() {
 		this.size(800, 600);
-		this.noLoop();
-	}
-
-	@Override
-	public void mouseClicked() {
-		this.redraw();
+		this.loop();
 	}
 
 	@Override
--- linn.examples/src/main/java/linn/examples/SierpinskiExample.java
+++ linn.examples/src/main/java/linn/examples/SierpinskiExample.java
@@ -1,44 +1,71 @@
+/**
+ * Copyright (c) 2016 by Thomas Trojer <thomas@trojer.net>
+ * LINN - A small L-System interpreter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package linn.examples;
 
 import linn.core.Linn;
 import linn.core.execute.LinnExecutor;
 import linn.core.execute.state.LinnTurtle;
 import linn.core.lang.LinnBuilder;
+import linn.core.math.Bounds;
 import processing.core.PApplet;
 
+/**
+ * A 2D processing sketch that draws a continuous line version of the Sierpinski
+ * triangle. This sketch features
+ * <ul>
+ * <li>new L-system production iteration on mouse click</li>
+ * <li>post-production rendering by using the history of state changes (traces)
+ * </li>
+ * <li>auto focus on draw area by making use of the computed bounds of a
+ * {@link LinnTurtle}</li>
+ * </ul>
+ *
+ * @author Thomas Trojer <thomas@trojer.net> -- Initial contribution
+ */
 public class SierpinskiExample extends PApplet {
 
-	private static double angle = 1 / 3.0 * Math.PI;
-	private static int len = 2;
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 800;
+
+	private static final double ANGLE = 1 / 3.0 * Math.PI;
+	private static final int LEN = 2;
 
 	private LinnExecutor linnExecutor;
 
 	@Override
 	public void setup() {
+		// the L-system definition
 		final Linn linn = LinnBuilder.newLinn("SierpinskiExample").withAuthor("Thomas Trojer")
 				// rule: A ---> B - A - B
-				.withRule("A").andProduction().F(len, "B").yaw(-angle).F(len, "A").yaw(-angle).F(len, "B").done()
+				.withRule("A").andProduction().F(LEN, "B").yaw(-ANGLE).F(LEN, "A").yaw(-ANGLE).F(LEN, "B").done()
 				// rule: B ---> A + B + A
-				.withRule("B").andProduction().F(len, "A").yaw(angle).F(len, "B").yaw(angle).F(len, "A").done()
+				.withRule("B").andProduction().F(LEN, "A").yaw(ANGLE).F(LEN, "B").yaw(ANGLE).F(LEN, "A").done()
 				// finalize
 				.build();
 		// configuring the execution environment
-		this.linnExecutor = LinnExecutor.newExecutor().useLinn(linn).traceStates(true).onStateChanged(t -> {
-			if (t.getPreviousState() == null) {
-				// await a second position to draw a line
-				return;
-			}
-			// connect previous and current position with a line
-			LinnTurtle tp = t.getPreviousState();
-			this.line((float) tp.getX(), (float) tp.getY(), (float) t.getX(), (float) t.getY());
-		})
+		this.linnExecutor = LinnExecutor.newExecutor().useLinn(linn).traceStates(true)
 				// axiom to start the L-System with: H
-				.withAxiom().F(len, "A").done();
+				.withAxiom().F(LEN, "A").done();
 	}
 
 	@Override
 	public void settings() {
-		this.size(800, 600);
+		this.size(WINDOW_WIDTH, WINDOW_HEIGHT);
 		this.noLoop();
 	}
 
@@ -51,6 +78,25 @@ public class SierpinskiExample extends PApplet {
 	public void draw() {
 		this.background(255);
 		this.linnExecutor.executeOnce();
+		// get post production state (and its trace)
+		LinnTurtle state = this.linnExecutor.getState();
+		LinnTurtle previousState = state.getPreviousState();
+		if (previousState == null) {
+			return;
+		}
+		Bounds bounds = state.getBounds();
+		// focus on actual drawing area
+		this.pushMatrix();
+		this.scale(WINDOW_WIDTH / (float) bounds.getExpansionX(), WINDOW_HEIGHT / (float) bounds.getExpansionY());
+		this.translate((float) -bounds.getMinX(), (float) -bounds.getMinY());
+		// render lines that connect states
+		while (previousState != null) {
+			this.line((float) state.getX(), (float) state.getY(), (float) previousState.getX(), (float) previousState.getY());
+			// update states
+			state = previousState;
+			previousState = state.getPreviousState();
+		}
+		this.popMatrix();
 	}
 
 	public static void main(String _args[]) {
